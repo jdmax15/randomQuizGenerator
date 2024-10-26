9faad9bd4d2f98373612b3274a3e053c56b86984
jdmax15 <joel.maxwell15@gmail.com>
2024-10-27 07:41:20 +1100

    1) Added code for generating file names for quiz and answer sheets.\n2) Added answer and wrong answer logic.

--- randomQuizGenerator.py
+++ randomQuizGenerator.py
@@ -63,9 +63,28 @@ capitals = {
 
 for quizNum in range(35):
     # TODO: Create the quic and answer key files.
+    quizFile = open(f'capitalsquiz{quizNum + 1}.txt', 'w')
+    answerKeyFile = open(f'capitalsquiz_answers{quizNum + 1}.txt', 'w')
 
     # TODO: Write out the header for the quiz.
+    quizFile.write('Name:\n\nDate:\n\nPeriod:\n\n')
+    quizFile.write((' ' * 20) + f'State Capitals Quiz (Form{quizNum + 1})')
+    quizFile.write('\n\n')
 
     # TODO: Shuffle the order of the states.
+    states = list(capitals.keys())
+    random.shuffle(states)
 
-    # TODO: :oop through all 50 states, making a question for each.
+    # TODO: Loop through all 50 states, making a question for each.
+    for questionNum in range(50):
+        # Get right and wrong answers.
+        correctAnswer = capitals[states[questionNum]]
+        wrongAnswers = list(capitals.values())
+        del wrongAnswers[wrongAnswers.index(correctAnswer)]
+        wrongAnswers = random.sample(wrongAnswers, 3)
+        answerOptions = wrongAnswers + [correctAnswer]
+        random.shuffle(answerOptions)
+
+        # TODO: Write the question and answer options to theh quiz file.
+
+        # TODO: Write the answer key to a file.
